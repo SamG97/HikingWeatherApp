409cc9a45fb46ca712e31aefb0125db41af8ecdd
Veraghin <Veraghin@users.noreply.github.com>
2017-05-15 19:08:47 +0100

    Added API, made singleton class using our api key

---
+++ src/APIcaller.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Dávid on 2017.05.11.
+ * Singleton class creating the owm client, it's only purpose is to
+ * instantiate the client with our own API-key, and keep it a singleton.
+ **/
+
+import org.bitpipeline.lib.owm.*;
+
+public class APIcaller extends OwmClient {
+
+
+    private static APIcaller sCaller;
+
+    private APIcaller(){
+        super();
+    }
+
+    public static APIcaller getClient(){
+        if ( sCaller == null ){
+            sCaller = new APIcaller();
+            sCaller.setAPPID("d12c4a04b7d0170dff8f1afca1e4c0ff");
+        }
+        return sCaller;
+    }
+
+}
