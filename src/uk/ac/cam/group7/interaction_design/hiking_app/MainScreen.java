e48469c41af31a8f1dec2dd3f64ace72c478dd94
alexfilip2 <acf61@cam.ac.uk>
2017-05-18 18:52:18 +0100

    Update MainScreen.java

--- src/uk/ac/cam/group7/interaction_design/hiking_app/MainScreen.java
+++ src/uk/ac/cam/group7/interaction_design/hiking_app/MainScreen.java
@@ -14,7 +14,11 @@ import javax.swing.border.EtchedBorder;
  * @author Alex
  */
 class MainScreen  {
-
+	
+   private static ForecastContainer sForecastContainer= new ForecastContainer();
+   
+    /** Specify the look and feel to use by defining the LOOKANDFEEL constant */
+   
     private static JFrame sWindow;
     private static SearchPanel sSearchPanel;
     private static JPanel sMainPanel;
@@ -23,30 +27,29 @@ class MainScreen  {
     /**
      * Constructor for the MainScreen
      */
-    public MainScreen() {
+    
+       private MainScreen() {
+    	
         sWindow = new JFrame("Weather app");
         sWindow.setPreferredSize(new Dimension(900,700));
+        sWindow.setLocation(240, 30);
         sWindow.setResizable(false);
         sWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+       
         sSearchPanel = new SearchPanel();
-
         sMainPanel = new JPanel();
         sMainPanel.setLayout(new BoxLayout(sMainPanel, BoxLayout.PAGE_AXIS));
-
+       
         JScrollPane scrollPane = new JScrollPane(sMainPanel);
         scrollPane.setPreferredSize(new Dimension(700,700));
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         scrollPane.getViewport().setOpaque(false);
 
-
-
         sWindow.add(scrollPane,BorderLayout.CENTER);
         sWindow.add(sSearchPanel,BorderLayout.NORTH);
-
         sWindow.pack();
-        scrollPane.requestFocusInWindow(); //Hack to make the Latitude textfield unfocused
         sWindow.setVisible(true);
 
         // sWindow Listeners
@@ -56,10 +59,13 @@ class MainScreen  {
 
             } //windowClosing
         } );
-
-
+        
+        
+        // initialize the main screen with the saved (recent) locations 
+        refreshMain();
+              
     }
-
+    
     /**
      * static! getter for the window
      * so other panels when called can hide or update it;
@@ -76,11 +82,28 @@ class MainScreen  {
      * @param input
      * the location we are adding
      */
-    public static void addScrollField(AddedLocation input)
+    public static void addNewLocation(AddedLocation input)
     {
-        sMainPanel.add(input);
+        sMainPanel.add(input);  
+        sWindow.setVisible(true);
+        sForecastContainer.addToRecent(input.getLoc());
     }
-
+  
+    
+    /** updating the main panel when new input from the API is recieved
+    * to be called when an update session is done 
+    */
+    
+    private void refreshMain()
+    {
+    	 
+        for(Location recentloc: sForecastContainer.getRecent())
+        {
+        	sMainPanel.add(new AddedLocation(recentloc));
+        }
+        sWindow.setVisible(true);
+    }
+    
     /**
      * Add a border to a component
      * @param component
@@ -93,14 +116,53 @@ class MainScreen  {
         Border tb = BorderFactory.createTitledBorder(etch,title);
         component.setBorder(tb);
     }
-
-
-    public static void main(String[] args) {
-        MainScreen app = new MainScreen();
+    
+    /** remove a location from the saved list  */
+    public static void removeLoc(AddedLocation location)
+    {
+    	
+    	sMainPanel.remove(location);
+    	sWindow.revalidate();
+    	sWindow.repaint();
+    	sForecastContainer.removeLocation(location.getLoc());   	
+    }
+    
+    /** rename a location from the saved list */
+    public static void renameLoc(AddedLocation location, String name)
+    {
+    	location.setText(name);
+    	sWindow.revalidate();
+    	sWindow.repaint();
+    	 	
+    }
+    
+    /** check if a location is already saved*/
+    public static boolean contains(Location location)
+    {
+    	return sForecastContainer.getRecent().contains(location);
     }
 
 
+    /** starting point of the application which creates the main screen*/
+    private static void createAndShowGUI() {	
+    	/**Set the look and feel */
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        //Create and set up the window.
+       MainScreen app = new MainScreen();
+       
+    }
+
+
+    public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+
+    		UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+   
+
+
 }
-
-
-
